10b3f6ff324a68a2310385eae3d6eabc9f7181ae
Spencer Pearson <speeze.pearson@gmail.com>
2025-02-20 03:00:17 -0800

    cleanup: dead code

--- src/util.ts
+++ src/util.ts
@@ -37,46 +37,6 @@ export function vavg(
     .divideScalar(weights[0] + weights[1]);
 }
 
-// export function getTopoSquare(
-//   cur: ByDancer<DancerState>,
-//   cornerId: DancerId,
-//   label1: keyof DancerState["Labels"],
-//   label2: keyof DancerState["Labels"]
-// ): {
-//   leftCounter: { id: DancerId; state: DancerState };
-//   rightCounter: { id: DancerId; state: DancerState };
-//   opposite: { id: DancerId; state: DancerState };
-// } {
-//   const dancer = cur.get(cornerId)!;
-//   const counterId1 = dancer.labels[label1]!;
-//   const counterId2 = dancer.labels[label2]!;
-//   const counter1 = cur.get(counterId1)!;
-//   const counter2 = cur.get(counterId2)!;
-//   const oppositeId = counter1.labels[label2]!;
-//   if (oppositeId !== counter2.labels[label1]) {
-//     throw new Error(
-//       `dancer ${cornerId} has ${label1}=${dancer.labels[label1]} and ${label2}=${dancer.labels[label2]} but ${counter1}'s ${label2} is ${counter1.labels[label2]} while ${counter2}'s ${label1} is ${counter2.labels[label1]}`
-//     );
-//   }
-//   const opposite = cur.get(oppositeId)!;
-
-//   const counter1IsRight = (() => {
-//     const toCounter1 = counter1.posn.clone().subtract(dancer.posn);
-//     const toCounter2 = counter2.posn.clone().subtract(dancer.posn);
-//     return toCounter1.cross(toCounter2) > 0;
-//   })();
-
-//   return {
-//     leftCounter: counter1IsRight
-//       ? { id: counterId2, state: counter2 }
-//       : { id: counterId1, state: counter1 },
-//     rightCounter: counter1IsRight
-//       ? { id: counterId1, state: counter1 }
-//       : { id: counterId2, state: counter2 },
-//     opposite: { id: oppositeId, state: opposite },
-//   };
-// }
-
 export function sameSideOfSet(p1: Victor, p2: Victor): boolean {
   return p1.x * p2.x > 0;
 }
